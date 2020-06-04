06af1bc4423790712cee81fa83836ed96b6f044e
Freddie Martinez <Freddie@ugps-MacBook.local>
2020-06-04 15:27:47 -0500

    End

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/**********************
+ * Name: Freddie Martinez
+ * Intro to Class
+ * Purpose: Creating a simple "Hello World" program to illustate
+ * understading of submission guidlines and github.
+ * Date: June 4, 2020
+ * *******************/
+
+
+/****************
+ * File: main.cpp
+ * Author: Freddie Martinez
+ ****************/
+
+#include <main.h>
+
+int main()
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,20 @@
+/**********************
+ * Name: Freddie Martinez
+ * Intro to Class
+ * Purpose: Creating a simple "Hello World" program to illustate
+ * understading of submission guidlines and github.
+ * Date: June 4, 2020
+ * *******************/
+
+
+/****************
+ * File: main.h
+ * Author: Freddie Martinez
+ ****************/
+
+#ifndef INTROCLASS_MAIN_H
+#define INTROCLASS_MAIN_H
+
+#include <iostream>
+
+#endif //NTROCLASS_MAIN_H
